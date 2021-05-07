114b34f89c2ca6dea00e56c0f9f406d3dde4aefe
OgGalZ <oggalze@gmail.com>
2021-05-07 17:15:37 +0200

    add Player

---
+++ src/main/java/me/oggalz/uhc_games/player/PlayerManager.java
@@ -0,0 +1,28 @@
+package me.oggalz.uhc_games.player;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class PlayerManager {
+
+    private final Map<UUID, Player> players;
+
+    public PlayerManager(UUID uuid) {
+        players = new HashMap<>();
+    }
+
+    public int getPlayers() {
+        return players.size();
+    }
+
+    public void addPlayer(UUID uuid) {
+        Player Player = new Player(uuid);
+        players.put(uuid, Player);
+    }
+
+    public Player getPlayer(UUID uuid) {
+        return players.get(uuid);
+    }
+
+}
