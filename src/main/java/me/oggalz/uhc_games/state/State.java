5fe3aaa43119c093fd16ca74e4df1c1f833d70c0
OgGalZ <oggalze@gmail.com>
2021-05-07 17:10:01 +0200

    add State.java

---
+++ src/main/java/me/oggalz/uhc_games/state/State.java
@@ -0,0 +1,6 @@
+package me.oggalz.uhc_games.state;
+
+public enum State {
+
+    WAITING, STARTING , PLAYING
+}
---
+++ src/main/java/me/oggalz/uhc_games/state/StateManager.java
@@ -0,0 +1,11 @@
+package me.oggalz.uhc_games.state;
+
+public class StateManager {
+
+    private State gameState;
+
+    public StateManager() {
+        gameState = State.WAITING;
+    }
+
+}
