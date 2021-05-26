03bc31e39c98d1013c7598cba2bf8f6c18818bfe
oggalz <oggalze@gmail.com>
2021-05-26 16:39:19 +0200

    test

--- src/main/java/me/oggalz/uhc_games/listeners/UtilsListeners.java
+++ src/main/java/me/oggalz/uhc_games/listeners/UtilsListeners.java
@@ -25,7 +25,6 @@ public class UtilsListeners implements Listener {
         this.playerManager = playerManager;
     }
 
-
     @EventHandler
     public void onFoodLevelChange(FoodLevelChangeEvent event) {
         if (stateManager.hasNotStarted()) {
