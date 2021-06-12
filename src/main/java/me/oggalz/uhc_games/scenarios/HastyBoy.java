77be498b4ea742e82a8cd90117c55a4da6231d52
oggalz <oggalze@gmail.com>
2021-06-12 11:36:04 +0200

    Review

--- src/main/java/me/oggalz/uhc_games/scenarios/HastyBoy.java
+++ src/main/java/me/oggalz/uhc_games/scenarios/HastyBoy.java
@@ -9,8 +9,31 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
 
-public class HastyBoy  implements Listener {
+import java.util.ArrayList;
+import java.util.List;
 
+public class HastyBoy implements Listener {
+
+    private final List<Material> materials = new ArrayList<>();
+
+    public HastyBoy() {
+        materials.add(UniversalMaterial.WOODEN_AXE.getType());
+        materials.add(UniversalMaterial.WOODEN_PICKAXE.getType());
+        materials.add(UniversalMaterial.WOODEN_SHOVEL.getType());
+        materials.add(UniversalMaterial.GOLDEN_AXE.getType());
+        materials.add(UniversalMaterial.GOLDEN_PICKAXE.getType());
+        materials.add(UniversalMaterial.GOLDEN_SHOVEL.getType());
+        materials.add(Material.STONE_AXE);
+        materials.add(Material.STONE_PICKAXE);
+        materials.add(UniversalMaterial.STONE_SHOVEL.getType());
+        materials.add(Material.IRON_AXE);
+        materials.add(Material.IRON_PICKAXE);
+        materials.add(UniversalMaterial.IRON_SHOVEL.getType());
+        materials.add(Material.DIAMOND_AXE);
+        materials.add(Material.DIAMOND_PICKAXE);
+        materials.add(UniversalMaterial.DIAMOND_SHOVEL.getType());
+
+    }
 
     @EventHandler
     public void playerCraftEvent(PrepareItemCraftEvent event) {
@@ -18,30 +41,12 @@ public class HastyBoy  implements Listener {
         if (event.getInventory().getResult() == null) return;
 
         Material itemType = event.getInventory().getResult().getType();
-
-        if (itemType != Material.DIAMOND_HOE &&
-                itemType != UniversalMaterial.WOODEN_AXE.getType() &&
-                itemType != UniversalMaterial.WOODEN_PICKAXE.getType() &&
-                itemType != UniversalMaterial.WOODEN_SHOVEL.getType() &&
-                itemType != UniversalMaterial.GOLDEN_AXE.getType() &&
-                itemType != UniversalMaterial.GOLDEN_PICKAXE.getType() &&
-                itemType != UniversalMaterial.GOLDEN_SHOVEL.getType() &&
-                itemType != Material.STONE_AXE &&
-                itemType != Material.STONE_PICKAXE &&
-                itemType != UniversalMaterial.STONE_SHOVEL.getType() &&
-                itemType != Material.IRON_AXE &&
-                itemType != Material.IRON_PICKAXE &&
-                itemType != UniversalMaterial.IRON_SHOVEL.getType() &&
-                itemType != Material.DIAMOND_AXE &&
-                itemType != Material.DIAMOND_PICKAXE &&
-                itemType != UniversalMaterial.DIAMOND_SHOVEL.getType()) {
-            return;
+        if (materials.contains(itemType)) {
+            ItemStack item = new ItemStack(itemType);
+            item.addUnsafeEnchantment(Enchantment.DIG_SPEED, 3);
+            item.addUnsafeEnchantment(Enchantment.DURABILITY, 2);
+            event.getInventory().setResult(item);
         }
-        ItemStack item = new ItemStack(itemType);
-        item.addUnsafeEnchantment(Enchantment.DIG_SPEED, 3);
-        item.addUnsafeEnchantment(Enchantment.DURABILITY, 2);
-        event.getInventory().setResult(item);
-
 
     }
 
